d4aba5b06942dbf8c1cda7410a651a08f555420b
santosh pattanaik <89644363+santosh-pattnaik@users.noreply.github.com>
2021-12-17 23:19:19 +0530

    Add files via upload

--- student.js
+++ student.js
@@ -1,34 +1,12 @@
-const fs = require('fs');
-//const studentjson = require('');
-module.exports = {
-    getStudent : (req,res)=>{
-        if(req.params['method']==='getDetails'){
-            res.write(JSON.stringify(req.body));
-            res.end();
-        }
-        if(req.params['method']==='studentList'){
-            res.write(JSON.stringify(req.body));
-            res.end();
-        }
-    },
-    postStudent : (req,res)=>{
-        if(req.params['method']==='add'){
-            console.log(req.body);
-            let data = req.body;
-            fs.writeFile('./controller/student_details.txt',JSON.stringify(data),err=>{
-                if(err) throw err;
-                console.log("Done Writing");
-            })
-        res.send('{"result" : "success"}');
-        }
-    },
-    putStudent : (req,res)=>{
+const expressClass = require('express');
+const studentRouter = expressClass.Router();
+const studentController = require('../controller/student.js');
 
-    },
-    deleteStudent : (req,res)=>{
 
-    },
-    patchStudent : (req,res)=>{
+studentRouter.get("/:method",studentController.getStudent)
+studentRouter.post("/:method",studentController.postStudent)
+studentRouter.put("/",studentController.putStudent)
+studentRouter.delete("/",studentController.deleteStudent)
+studentRouter.patch("/",studentController.patchStudent)
 
-    }
-}
+module.exports = studentRouter;
